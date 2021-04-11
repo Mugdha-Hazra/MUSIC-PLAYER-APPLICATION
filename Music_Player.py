18072a9bee7a54ed7ad0dea9b42e5e113465557b
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-04-11 11:09:07 +0530

    Update Music_Player.py

--- Music_Player.py
+++ Music_Player.py
@@ -1,14 +1,39 @@
 '''
-module included
-1. Pygame : for importing sounds libraries and computer graphics(its a module for creating video games) 
-2. Tkinter : for application window
-3. Os : for intracting with the os
+It's a simple music player appliaction build using python 
+It has various functionalities like:
+1.choose a song from the play list 
+2.play a song 
+3.pause a song
+4.unpause the song 
+5.stop a song from playing and select another song from playist.
+6.we can resize the application according to our need.
 '''
+'''
+module included
+=================
+1. Pygame : for importing sounds libraries and computer graphics(its a module for creating video games) 
+2. Tkinter : for application window using gui
+  -> from Tkinter module we will import filedialogue : used for open and   save function.
+  ->Askdirectory from filedialog : it provides a popup window to choose a directory from your window.
+3. Os : for intracting with the os
+
+features:
+==========
+1. create a application window
+2. add a title to application window
+3. set the size of the application window
+
+Installation
+=============
+pygame doesnt come with python installation we have to install it seperately.
+  in terminal 
+  pip install pygame
+'''
+
 import pygame
 import tkinter as tkr
 from tkinter.filedialog import askdirectory
 import os
-
 musicplayer = tkr.Tk()
 musicplayer.title("Music Player")
 musicplayer.geometry("450x450")
